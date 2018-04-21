d391bacbbe64edd9550c0b17843cb88b9e6d2242
Kinh Dom <kinhdomcom@gmail.com>
2018-04-21 16:40:32 +0700

    thua file chitiet js

--- public/css/chitiet.js
+++ public/css/chitiet.js
@@ -1,3 +1,3 @@
-$(document).ready(function(){$('.collapsible').collapsible();$(".owl-carousel").owlCarousel({stagePadding:50,loop:true,margin:20,responsive:{0:{items:1},600:{items:2},1000:{items:2}}});$('.carousel').carousel({fullWidth:true,indicators:true,padding:6,numVisible:5});$('.tabs').tabs();$('.list-color ul li span').click(function(e){$(this).parent().addClass('selected').siblings().removeClass('selected');$('.txt-color').text(e.target.attributes[1].value)
-$('#xe').attr('src',e.target.attributes[0].value)})
-if(!/\w/.test($('.title_ngoai_that').text().trim())){$('#ngoai_that').hide()}if(!/\w/.test($('.title_noi_that').text().trim())){$('#noi_that').hide()}});
+// $(document).ready(function(){$('.collapsible').collapsible();$(".owl-carousel").owlCarousel({stagePadding:50,loop:true,margin:20,responsive:{0:{items:1},600:{items:2},1000:{items:2}}});$('.carousel').carousel({fullWidth:true,indicators:true,padding:6,numVisible:5});$('.tabs').tabs();$('.list-color ul li span').click(function(e){$(this).parent().addClass('selected').siblings().removeClass('selected');$('.txt-color').text(e.target.attributes[1].value)
+// $('#xe').attr('src',e.target.attributes[0].value)})
+// if(!/\w/.test($('.title_ngoai_that').text().trim())){$('#ngoai_that').hide()}if(!/\w/.test($('.title_noi_that').text().trim())){$('#noi_that').hide()}});
